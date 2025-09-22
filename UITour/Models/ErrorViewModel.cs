c5116d5992d4e6d7d67c69fbe2cd1bdc23a54cde
hoang <hoang@TAI>
2025-09-22 18:01:54 +0700

    Add project files.

---
+++ UITour/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UITour.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
